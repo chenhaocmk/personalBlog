8523cdfd1965d269648c40f5e4fed78bab8a0e89
Hao Chen <chenhao_ch@outlook.com>
2019-02-16 00:15:20 -0800

    Basic HTTP server
    curl localhost:8888

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ var server = require("./server");
 var route = require("./route");
 var requestHandlers = require("./requestHandlers");
 
-const API_PREFIX = '/api';
+const API_PREFIX = "/";
 var handle = {};
+
+handle[API_PREFIX + 'blog_list'] = requestHandlers.getBlogs;
+// handle[API_PREFIX + 'blog'] = requestHandlers.getBlogDetail;
+// handle[API_PREFIX + '']
+
+server.start(handle);
--- server.js
+++ server.js
@@ -2,8 +2,24 @@
 var http = require("http");
 var url = require("url");
 
-function start() {
+function start(handle) {
+    function onRequest(request, response) {
+      var pathName = url.parse(request.url).pathname;
+      console.log("Request for " + pathName + " received.");
 
+      request.setEncoding("utf8");
+
+      if (pathName in handle) {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write("Hello World");
+      } else {
+        response.writeHead(404, {"Content-Type": "text/plain"});
+        response.write("Not found!");
+      }
+      response.end();
+    }
+    http.createServer(onRequest).listen(8888);
+    console.log("Server has started.");
 }
 
 exports.start = start;
